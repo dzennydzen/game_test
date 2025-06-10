55503445aaef0b10a8f3060284805618e92af63e
Dzenny <batterflyfly96@gmail.com>
2025-06-10 21:52:07 +0300

    Move ESLint config to eslint.config.mjs and remove old config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,27 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    ignores: ["node_modules/**", "dist/**"],
+    plugins: { js },
+    extends: ["js/recommended"],// базовый набор правил
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    rules: {
+      "no-restricted-syntax": [
+        "error",
+        "LabeledStatement",
+        "WithStatement"
+      ],
+      "no-unused-vars": ["error"],
+      "indent": ["error", 2],
+      "no-multi-spaces": ["error"],
+      "no-mixed-spaces-and-tabs": "error",
+    },
+  },
 ]);
